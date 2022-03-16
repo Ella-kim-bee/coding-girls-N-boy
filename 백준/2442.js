7eb4b0d3c43937d7c737762c9bb65924972e4e35
Ella-kim <77561871+Ella-kim-bee@users.noreply.github.com>
2022-03-16 19:21:52 +0900

    Update 2442.js

--- 백준/2442.js
+++ 백준/2442.js
@@ -1,2 +1,28 @@
 // https://www.acmicpc.net/problem/2442, 별 찍기 -5
 // node 2442
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (a) {
+
+    /**
+     * 백준 컴퓨터가 a에다가 변수 넣어줌.
+     * 아래에 코드를 작성하세요.
+     */
+
+    // 코드 작성, 매개변수 a
+
+    let output = '';
+    for (let i = 1; i <= a; i++) {
+      output += ' '.repeat(a - i) + '*'.repeat(i * 2 - 1) + '\n'
+    }
+
+    console.log(output);
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
